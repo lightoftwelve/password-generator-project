8f431e8101d2377025b549d2ac27f26ca582b074
lightoftwelve <info@lightoftwelve.com>
2023-06-19 13:12:27 -0400

    Initialized password and selectedChars strings, and implemented prompt for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
+// Choices for character sets for password
 var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbericChars = "0123456789";
 var specialChars = "~!@#$%^&*()_+-=[]{}|:;<,?>./";
+
+// Start of function to generate the password
+function generatePassword() {
+  var password = ""; //Initialized an empty string to hold the generated password
+  var selectedChars = ""; // Initialized an empty string to hold selected characters for the password
+
+  var passwordLength = prompt("Please enter a password length between 8 - 128 characters:"); // Asks the user to input a password length
+
+  // Now to write how to handle the users input including character types and error checking and finish the generating the password function
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
